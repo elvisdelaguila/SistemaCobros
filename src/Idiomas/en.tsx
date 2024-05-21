2d9620e1bb1b62847e6f1df033ddfd7dca8c29ea
elvisdelaguila@gmail.com <123422903+elvisdelaguila@users.noreply.github.com>
2024-05-20 22:18:00 -0500

    Instalando Idiomas

---
+++ src/Idiomas/en.tsx
@@ -0,0 +1,10 @@
+export default {
+    /*ScreenLogin*/
+    varlblUsuario: 'User',
+    vartxtUsuario: 'Enter User',
+    varlblClave: 'Password',
+    vartxtClave: 'Enter Password',
+    varlblNoTienesCuenta: 'Don\'t have an account? Create it',
+    vartxtNoTienesCuenta: 'Here',
+    varlblMensajeOlvidaste: 'Forgot your password?',  
+  };
---
+++ src/Idiomas/es.tsx
@@ -0,0 +1,10 @@
+export default {
+    /*ScreenLogin*/
+    varlblUsuario: 'Usuario',
+    vartxtUsuario: 'Ingrese Usuario',
+    varlblClave: 'Clave',
+    vartxtClave: 'Ingrese Clave',
+    varlblNoTienesCuenta: 'No tienes cuenta ? créala',
+    vartxtNoTienesCuenta: 'Aqui',
+    varlblMensajeOlvidaste: 'Olvidaste tu clave ? ',    
+  };
